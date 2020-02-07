e8a38da2e7752c1cdddb1113adfcbd5b3992f648
giovagr20 <giovannyg32@gmail.com>
2020-02-07 11:22:13 -0500

    Add migrations, database and controller taxis

---
+++ TaxiQualifier.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaxiQualifier.Web.Data.Entities;
+
+namespace TaxiQualifier.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
--- TaxiQualifier.Web/Startup.cs
+++ TaxiQualifier.Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TaxiQualifier.Web.Data;
 
 namespace TaxiQualifier.Web
 {
@@ -31,6 +33,10 @@ namespace TaxiQualifier.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
