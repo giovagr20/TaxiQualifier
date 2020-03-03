87cf56a42de79f47d92367092268463d8483b9bc
giovagr20 <giovannyg32@gmail.com>
2020-03-03 08:55:14 -0500

    Add Splasj, Icon on App

---
+++ TaxiQualifier.Prism/TaxiQualifier.Prism.Android/SplashActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using System.Threading;
+
+namespace TaxiQualifier.Prism.Droid
+{
+    [Activity(
+        Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Thread.Sleep(1800);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
